079e1d858525183806c130700d321bfe20b5d3e8
Thibault <thibaultdave@hotmail.fr>
2026-09-17 16:00:40 +0200

    Add 401 JWT tests

--- src/test/java/com/gdu/wacdo/securitiesTest/JwtAuthenticationFilterTest.java
+++ src/test/java/com/gdu/wacdo/securitiesTest/JwtAuthenticationFilterTest.java
@@ -1,9 +1,13 @@
 package com.gdu.wacdo.securitiesTest;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.gdu.wacdo.builders.ErrorResponseBuilder;
+import com.gdu.wacdo.dto.ErrorResponseDTO;
 import com.gdu.wacdo.securities.JwtAuthenticationFilter;
 import com.gdu.wacdo.securities.JwtService;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.User;
@@ -136,5 +140,90 @@ class JwtAuthenticationFilterTest {
         );
 
         verify(filterChain).doFilter(request, response);
+    }
+
+    @Test
+    void doFilter_shouldReturn401_whenTokenIsExpired() throws Exception {
+
+        doFilter_shouldReturn401(
+                "expired-token",
+                "JWT token has expired.",
+                "error.jwt.expired",
+                new TokenExpiredException(
+                        "JWT token has expired.",
+                        null
+                )
+        );
+    }
+
+    @Test
+    void doFilter_shouldReturn401_whenTokenIsInvalid() throws Exception {
+
+        doFilter_shouldReturn401(
+                "invalid-token",
+                "Invalid JWT token.",
+                "error.jwt.invalid",
+                new JWTVerificationException(
+                        "Invalid JWT token."
+                )
+        );
+    }
+
+    void doFilter_shouldReturn401(
+            String token,
+            String errorMessage,
+            String errorMessageKey,
+            JWTVerificationException exception
+    )  throws Exception {
+
+        int status = HttpServletResponse.SC_UNAUTHORIZED;
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        request.addHeader(
+                "Authorization",
+                "Bearer " + token
+        );
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        ErrorResponseDTO errorResponse =
+                new ErrorResponseDTO(
+                        status,
+                        errorMessage
+                );
+
+        when(jwtService.extractEmail(token))
+                .thenThrow(exception);
+
+        when(errorResponseBuilder.build(
+                status,
+                errorMessageKey
+        )).thenReturn(errorResponse);
+
+        filter.doFilter(
+                request,
+                response,
+                filterChain
+        );
+
+        assertEquals(
+                status,
+                response.getStatus()
+        );
+
+        verify(errorResponseBuilder).build(
+                status,
+                errorMessageKey
+        );
+
+        verify(filterChain, never())
+                .doFilter(request, response);
+
+        assertNull(
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+        );
     }
 }
